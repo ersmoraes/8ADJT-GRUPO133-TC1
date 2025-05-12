6af22557b553ee8be627d2036b9d7f6174603910
Eric Moraes <eric_moraes20@outlook.com>
2025-05-12 20:50:51 -0300

    ajustes e novas implementações

--- src/main/java/br/com/fiap/techChallenge/restaurante_api/api/dto/response/LoginResponseDTO.java
+++ src/main/java/br/com/fiap/techChallenge/restaurante_api/api/dto/response/LoginResponseDTO.java
@@ -1,4 +1,12 @@
 package br.com.fiap.techChallenge.restaurante_api.api.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class LoginResponseDTO {
+
+    private String token;
+    private String type;
 }
--- src/main/java/br/com/fiap/techChallenge/restaurante_api/security/JwtTokenProvider.java
+++ src/main/java/br/com/fiap/techChallenge/restaurante_api/security/JwtTokenProvider.java
@@ -1,4 +1,38 @@
 package br.com.fiap.techChallenge.restaurante_api.security;
 
+import br.com.fiap.techChallenge.restaurante_api.domain.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtTokenProvider {
+
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${security.jwt.expiration}")
+    private long jwtExpirationMs;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getLogin())
+                .claim("userId", user.getId())
+                .claim("userType", user.getUserType().name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
