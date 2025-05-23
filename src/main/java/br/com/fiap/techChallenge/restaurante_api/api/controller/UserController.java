8d56c9534e7d286bb3c10148d2b7a846b6ee415f
teteretu <teteretu@yahoo.com>
2025-05-23 15:58:42 -0300

    removendo imports desnecessários

--- src/main/java/br/com/fiap/techChallenge/restaurante_api/api/controller/UserController.java
+++ src/main/java/br/com/fiap/techChallenge/restaurante_api/api/controller/UserController.java
@@ -19,9 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
